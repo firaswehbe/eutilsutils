885a8475542483cedd494fe4980a8c0b48dcedc3
Firas Wehbe <firaswehbe@users.noreply.github.com>
2016-04-02 19:18:34 -0500

    Add scripts/pmid2csv using pandas

--- scripts/pmid2csv.py
+++ scripts/pmid2csv.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
 from Bio import Entrez
-from Bio import Medline
+import csv
+import pandas as pd
 import sys
 import os.path
+
+print sys.argv
 
 if len(sys.argv) < 3:
     sys.exit('Need to specify 2 arguments')
@@ -16,10 +19,26 @@ csvfile = sys.argv[2]
 if os.path.exists(csvfile):
     sys.exit('Output file "{0}" already exist, cannot overwrite'.format(csvfile))
 
+# Defining the columns, eSummary is flat, so no need to fetch data 
+# from nested structures
+
+cols =[
+    'Id','Title','Source','SO'
+    ]
+
+outframe = pd.DataFrame( columns = cols )
+
+# Expect this to run in a terminal, so show dynamic results in bold red
+def makeRed(myinput):
+    return "\x1b[0;31m"+str(myinput)+"\x1b[0m"
+
+def makeYellow(myinput):
+    return "\x1b[0;33m"+str(myinput)+"\x1b[0m"
+
 
 # Need to tell the NLM who you are when you access e-utils
 Entrez.email = 'firaswehbe@users.noreply.github.com'
-Entrez.tool = 'github.com/firaswehbe/eutilsutils/scripts/bioPythonHelloWorld.py'
+Entrez.tool = 'github.com/firaswehbe/eutilsutils/scripts/pmid2csv.py'
 
 # Read a list of pubmed ids into a list for use in testing scripts
 testpmids = list()
@@ -27,26 +46,26 @@ fh = open(pmidfile,'r')
 for x in fh:
     testpmids.append(x.strip())
 fh.close()
-#print("Read ids from {0}: {1}\n..."\
-#        .format(makeYellow(pmidfile),makeRed(len(testpmids))))
+print("Read ids from {0}: {1}\n..."\
+        .format(makeYellow(pmidfile),makeRed(len(testpmids))))
 
 # Run an Entrez.post
-print("Sending {0} from the pmids file\n...".format('Entrez.epost'))
+print("Sending {0} from the pmids file\n...".format(makeYellow('Entrez.epost()')))
 myhandle = Entrez.epost( 'pubmed', id=','.join(testpmids) )
 myresult = Entrez.read( myhandle )
 mywebenv = myresult['WebEnv']
 myquerykey = myresult['QueryKey']
-#print("WebEnv: {0}\nquery_key: {1}\n..."\
-#        .format(makeRed(mywebenv),makeYellow(myquerykey)))
+print("WebEnv: {0}\nquery_key: {1}\n..."\
+        .format(makeRed(mywebenv),makeRed(myquerykey)))
 
 # Run an Entrez.esummary
-print("Requesting {0} based on the epost\n...".format('Entrez.esummary()'))
+print("Requesting {0} based on the epost\n...".format(makeYellow('Entrez.esummary()')))
 myhandle = Entrez.esummary( db='pubmed', webenv=mywebenv, query_key=myquerykey)
 myresult = Entrez.read( myhandle )
-print("Got response of length: {0}".format(len(myresult)))
-for x in myresult:
-    print("{0}\t{1}\t{2}\t{3}"\
-            .format(x['Id'],x['Title'][0:40],x['Source'],x['SO']))
-print("...")
+print("Got response of length: {0}".format(makeRed(len(myresult))))
+outframe = outframe.append( pd.DataFrame( myresult, columns = cols) , ignore_index=True)
 
-print sys.argv
+# Write to csvfile
+print("Writing to {0}\n...".format(makeRed(csvfile)))
+outframe.to_csv(csvfile, quoting=csv.QUOTE_ALL, index=False)
+
