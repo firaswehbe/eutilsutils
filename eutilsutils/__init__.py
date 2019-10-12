b32a55199b9fd7f11d34ad2c6ef732cc6ae35030
Firas Wehbe <firaswehbe@users.noreply.github.com>
2019-10-12 15:12:59 -0500

    Using app factory for flask app

--- eutilsutils/__init__.py
+++ eutilsutils/__init__.py
@@ -1,6 +1,25 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+import os
 
-@app.route('/')
-def home():
-    return 'There are {0} papers in the database.'.format( 'XX' )
+def create_app(): 
+    app = flask.Flask(__name__,instance_relative_config=True)
+    app.logger.info('initiated')
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    try:
+        app.config.from_pyfile('application.cfg')
+    except:
+        app.logger.error('Could not open config file')
+        flask.abort(500)
+
+
+    @app.route('/')
+    def home():
+        app.logger.info('home')
+        return 'There are {0} papers in the database.'.format( 'XX' )
+
+    return app
