8900a5b2567e4d32f1a4df638ffc1594300c99c8
Firas Wehbe <firaswehbe@users.noreply.github.com>
2022-12-16 17:18:53 -0600

    wip example three-step query

--- loadpmids.py
+++ loadpmids.py
@@ -1,19 +1,43 @@
 #!/usr/bin/env python
 
+import rich
 import rich.pretty
 import eutilsutils
 import eutilsutils.parser
 app = eutilsutils.EutilsUtils()
+import time
 
-with app.en.esearch(db='pubmed', term='wehbe f[au]', usehistory='y') as sh1:
-    result1 = app.en.read(sh1)
-    print(f"Search #{result1['QueryKey']} returns {result1['Count']} / WebEnv = {result1['WebEnv']}")
+mylog = []
 
-with app.en.efetch(db='pubmed', query_key=result1['QueryKey'], WebEnv=result1['WebEnv'], retmax=50) as sh2:
-    result2 = app.en.read(sh2)
+def append_log(theresult):
+    mylog.append((
+        theresult['Count'],
+        theresult['WebEnv'],
+        theresult['QueryKey']
+    ))
 
-thepaperinstances = eutilsutils.parser.loadEfetch2PMPapers(app,result2,False)
+with app.en.esearch(db='pubmed', term='Heart Failure', usehistory='y') as sh:
+    result = app.en.read(sh)
+    append_log(result)
 
-rich.pretty.pprint(thepaperinstances)
+with app.en.esearch(db='pubmed', term='artificial intelligence OR machine learning OR deep learning', usehistory='y', WebEnv=result['WebEnv']) as sh:
+    result = app.en.read(sh)
+    append_log(result)
+
+with app.en.esearch(db='pubmed', term='(#1) AND (#2) AND english[la]', usehistory='y', WebEnv=result['WebEnv']) as sh:
+    result = app.en.read(sh)
+    append_log(result)
+
+rich.pretty.pprint(mylog)
+
+step = 500
+load_log = []
+for i in range(0,int(result['Count']),step):
+    rich.print(f"Requesting {i} to {i+step} ...", end=" ")
+    with app.en.efetch(db='pubmed', query_key=result['QueryKey'], WebEnv=result['WebEnv'], retstart=i, retmax=step) as fh:
+        fetched = app.en.read(fh)
+    rich.print(f"Parsing {len(fetched['PubmedArticle'])}",end=" ")
+    load_log.extend(eutilsutils.parser.loadEfetch2PMPapers(app,fetched,False))
+    rich.print(f"Parsed")
+    time.sleep(3)
     
-
