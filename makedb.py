26b34834fb9985e66a3e0939787f2ccec3003e6f
Firas Wehbe <firaswehbe@users.noreply.github.com>
2022-12-15 23:42:42 -0600

    makedb now uses the package

--- makedb.py
+++ makedb.py
@@ -1,33 +1,11 @@
 #!/usr/bin/env python
 
-#######################################
-# DB Setup
-# Use this to create engine, session, and Base
+# Consider adding argparse for argument and rich output
 
-import sqlalchemy as SA
-import sqlalchemy.orm as SAORM
+from eutilsutils import EutilsUtils
+from eutilsutils.model import EutilsUtilsBaseModel
 
-#should use proper config module or something
-myengineurl = 'postgresql://eutilsutils_user:xxx@localhost:5432/eutilsutils_db'
-myengine = SA.create_engine(myengineurl)
-# look into scoped_session
-mysession = SAORM.sessionmaker(myengine)
-Base = SAORM.declarative_base()
+app = EutilsUtils()
 
-
-#######################################
-# Model definitions
-# Should use DB set up above to have access to Base
-
-class PMPaper(Base):
-    __tablename__ = 'pm_paper'
-
-    id = SA.Column(SA.Integer, primary_key=True)
-    title = SA.Column(SA.String(500))
-    pdat = SA.Column(SA.Date)
-    edat = SA.Column(SA.Date)
-
-#######################################
-# Do the thing
-
-Base.metadata.create_all(bind=myengine)
+EutilsUtilsBaseModel.metadata.drop_all(bind=app.db.engine)
+EutilsUtilsBaseModel.metadata.create_all(bind=app.db.engine)
